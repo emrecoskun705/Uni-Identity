9df8c9b4e92aed291b0a775121de28a7770d873c
emrecoskun705 <emrecoskun705@gmail.com>
2024-01-19 23:33:53 +0300

    add user role create method

--- src/API/UniIdentity.Domain/Users/UserRole.cs
+++ src/API/UniIdentity.Domain/Users/UserRole.cs
@@ -2,10 +2,22 @@
 
 namespace UniIdentity.Domain.Users;
 
-public class UserRole
+public sealed class UserRole
 {
-    public UserId UserId { get; set; }
-    public RoleId RoleId { get; set; }
-    public User User { get; set; } = null!;
-    public Role Role { get; set; } = null!;
+    public UserId UserId { get; private set; }
+    public RoleId RoleId { get; private set; }
+    public User User { get; private set; } = null!;
+    public Role Role { get; private set; } = null!;
+
+    private UserRole(UserId userId, RoleId roleId)
+    {
+        UserId = userId;
+        RoleId = roleId;
+    }
+
+    public static UserRole Create(UserId userId, RoleId roleId)
+    {
+        var userRole = new UserRole(userId, roleId); 
+        return userRole;
+    }
 }
