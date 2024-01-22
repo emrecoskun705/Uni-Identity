6bbd766a3dd81ad2547607e35111f8cd279771e4
emrecoskun705 <emrecoskun705@gmail.com>
2024-01-23 00:43:02 +0300

    add create password credential

--- src/API/UniIdentity.Domain/Credentials/PasswordCredential.cs
+++ src/API/UniIdentity.Domain/Credentials/PasswordCredential.cs
@@ -3,10 +3,25 @@ using UniIdentity.Domain.Users;
 
 namespace UniIdentity.Domain.Credentials;
 
-public class PasswordCredential : Credential
+public sealed class PasswordCredential : Credential
 {
-    public PasswordCredential(CredentialId id, UserId userId, DateTimeOffset createdDateTime, string? secretData, string? credentialData, short priority) : base(id, userId, createdDateTime, secretData, credentialData, priority)
+    private PasswordCredential(CredentialId id, UserId userId, DateTimeOffset createdDateTime, string? secretData, string? credentialData, short priority) : base(id, userId, createdDateTime, secretData, credentialData, priority)
     {
         Type = CredentialType.Password;
     }
+
+    public static PasswordCredential Create(UserId userId, DateTimeOffset createdDateTime, string password)
+    {
+        var secretData = PasswordManager.HashPassword(password);
+        
+        var credential = new PasswordCredential(CredentialId.New(), userId, createdDateTime, secretData, null, 10);
+
+        return credential;
+    }
+
+    public bool VerifyPassword(string password)
+    {
+        return PasswordManager.VerifyHashedPassword(SecretData!, password);
+    }
+    
 }
