c41081ef56525021d4b5081d272bf513efa063b8
emrecoskun705 <emrecoskun705@gmail.com>
2024-01-26 22:22:05 +0300

    update - use var type in PasswordManager.cs

--- src/API/UniIdentity.Domain/Credentials/PasswordManager.cs
+++ src/API/UniIdentity.Domain/Credentials/PasswordManager.cs
@@ -9,18 +9,15 @@ public sealed class PasswordManager
     {
         byte[] salt;
         byte[] buffer2;
-        if (password == null)
-        {
-            throw new ArgumentNullException(nameof(password));
-        }
+        ArgumentNullException.ThrowIfNull(password);
 
-        using (Rfc2898DeriveBytes bytes = new Rfc2898DeriveBytes(password, 0x10, 0x3e8, HashAlgorithmName.SHA256))
+        using (var bytes = new Rfc2898DeriveBytes(password, 0x10, 0x3e8, HashAlgorithmName.SHA256))
         {
             salt = bytes.Salt;
             buffer2 = bytes.GetBytes(0x20);
         }
 
-        byte[] dst = new byte[0x31];
+        var dst = new byte[0x31];
         Buffer.BlockCopy(salt, 0, dst, 1, 0x10);
         Buffer.BlockCopy(buffer2, 0, dst, 0x11, 0x20);
         return Convert.ToBase64String(dst);
@@ -39,17 +36,17 @@ public sealed class PasswordManager
             throw new ArgumentNullException(nameof(password));
         }
 
-        byte[] src = Convert.FromBase64String(hashedPassword);
+        var src = Convert.FromBase64String(hashedPassword);
         if ((src.Length != 0x31) || (src[0] != 0))
         {
             return false;
         }
 
-        byte[] dst = new byte[0x10];
+        var dst = new byte[0x10];
         Buffer.BlockCopy(src, 1, dst, 0, 0x10);
-        byte[] buffer3 = new byte[0x20];
+        var buffer3 = new byte[0x20];
         Buffer.BlockCopy(src, 0x11, buffer3, 0, 0x20);
-        using (Rfc2898DeriveBytes bytes = new Rfc2898DeriveBytes(password, dst, 0x3e8, HashAlgorithmName.SHA256))
+        using (var bytes = new Rfc2898DeriveBytes(password, dst, 0x3e8, HashAlgorithmName.SHA256))
         {
             buffer4 = bytes.GetBytes(0x20);
         }
