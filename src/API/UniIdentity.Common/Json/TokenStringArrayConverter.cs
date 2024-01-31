4bc2533eff86d3f0391f420f646075b185709730
emrecoskun705 <emrecoskun705@gmail.com>
2024-02-01 00:46:54 +0300

    add common project, add common to domain project and TokenStringArrayConverter

---
+++ src/API/UniIdentity.Common/Json/TokenStringArrayConverter.cs
@@ -0,0 +1,32 @@
+﻿namespace UniIdentity.Common.Json;
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+public class TokenStringArrayConverter : JsonConverter<string[]>
+{
+    public override string[]? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        using var doc = JsonDocument.ParseValue(ref reader);
+        var root = doc.RootElement;
+
+        if (root.ValueKind == JsonValueKind.Array)
+        {
+            return root.EnumerateArray().Select(e => e.GetString()).ToArray();
+        }
+
+        return Array.Empty<string>();
+    }
+
+    public override void Write(Utf8JsonWriter writer, string[] value, JsonSerializerOptions options)
+    {
+        writer.WriteStartArray();
+
+        foreach (var item in value)
+        {
+            writer.WriteStringValue(item);
+        }
+
+        writer.WriteEndArray();
+    }
+}
