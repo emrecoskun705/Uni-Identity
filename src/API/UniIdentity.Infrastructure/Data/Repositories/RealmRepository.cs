53c9a4bcf77903fa640e3dd6d6f1e1323ea39f2e
emrecoskun705 <emrecoskun705@gmail.com>
2024-02-11 00:27:29 +0300

    add realm repository implementation

---
+++ src/API/UniIdentity.Infrastructure/Data/Repositories/RealmRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UniIdentity.Domain.Realms;
+
+namespace UniIdentity.Infrastructure.Data.Repositories;
+
+internal class RealmRepository : Repository<Realm>, IRealmRepository
+{
+    public RealmRepository(ApplicationDbContext dbContext)
+        : base(dbContext)
+    {
+    }
+    
+    public async Task<Realm?> GetByRealmId(RealmId realmId, CancellationToken ct = default)
+    {
+        return await _db.Realm.FirstOrDefaultAsync(x => x.Id == realmId, cancellationToken: ct);
+    }
+
+}
