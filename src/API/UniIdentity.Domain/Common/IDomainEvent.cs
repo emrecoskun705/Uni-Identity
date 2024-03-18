1ebe71e41d55f10ea8b95b695adf4b2da7138f9e
emrecoskun705 <emrecoskun705@gmail.com>
2024-03-18 21:15:00 +0300

    fix typo in IDomainEvent file name, add IDomainEventHandler

--- src/API/UniIdentity.Domain/Common/IDomainEvent.cs
+++ src/API/UniIdentity.Domain/Common/IDomainEvent.cs
@@ -2,6 +2,9 @@
 
 namespace UniIdentity.Domain.Common;
 
+/// <summary>
+/// Represents the interface for an event that is raised within the domain.
+/// </summary>
 public interface IDomainEvent : INotification
 {
 }
---
+++ src/API/UniIdentity.Domain/Common/IDomainEventHandler.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace UniIdentity.Domain.Common;
+
+/// <summary>
+/// Represents a domain event handler interface.
+/// </summary>
+/// <typeparam name="TDomainEvent">The domain event type.</typeparam>
+public interface IDomainEventHandler<in TDomainEvent> : INotificationHandler<TDomainEvent>
+    where TDomainEvent : IDomainEvent
+{
+}
