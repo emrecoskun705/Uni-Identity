638b57e92c50d93f6bfddf5c3f92834c6238be8d
emrecoskun705 <emrecoskun705@gmail.com>
2024-04-01 23:40:28 +0300

    update rsa generation config with RSA configuration variables

---
+++ src/API/UniIdentity.Domain/Configs/Enums/KeyUse.cs
@@ -0,0 +1,42 @@
+﻿namespace UniIdentity.Domain.Configs.Enums;
+
+/// <summary>
+/// Represents the intended use of a cryptographic key.
+/// </summary>
+public record KeyUse
+{
+    public string Value { get; }
+
+    private KeyUse(string value) => Value = value;
+
+    /// <summary>
+    /// Gets a <see cref="KeyUse"/> instance representing the 'SIG' key use.
+    /// </summary>
+    public static KeyUse Sig => new KeyUse("SIG");
+
+    /// <summary>
+    /// Gets a <see cref="KeyUse"/> instance representing the 'ENC' key use.
+    /// </summary>
+    public static KeyUse Enc => new KeyUse("ENC");
+
+    /// <summary>
+    /// Implicitly converts a string value to a <see cref="KeyUse"/> instance.
+    /// </summary>
+    /// <param name="value">The string value representing the key use.</param>
+    /// <returns>A <see cref="KeyUse"/> instance corresponding to the provided string value.</returns>
+    /// <exception cref="ArgumentException">Thrown when the provided string value is invalid.</exception>
+    public static implicit operator KeyUse(string value) => value switch
+    {
+        "SIG" => Sig,
+        "ENC" => Enc,
+        _ => throw new ArgumentException($"Invalid value '{value}' for KeyUse"),
+    };
+
+    /// <summary>
+    /// Returns the string representation of the key use.
+    /// </summary>
+    /// <returns>The string value representing the key use.</returns>
+    public override string ToString() => Value;
+    
+    
+}
--- src/API/UniIdentity.Domain/Configs/RsaGenerationConfig.cs
+++ src/API/UniIdentity.Domain/Configs/RsaGenerationConfig.cs
@@ -1,4 +1,6 @@
-﻿using System.Security.Cryptography;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Cryptography;
+using UniIdentity.Domain.Configs.Enums;
 using UniIdentity.Domain.Configs.ValueObjects;
 using UniIdentity.Domain.Realms;
 
@@ -10,9 +12,36 @@ namespace UniIdentity.Domain.Configs;
 public sealed class RsaGenerationConfig : Config
 {
     // RSA Configuration attributes
-    private const string PublicKeyName = "publicKey";
     private const string PrivateKeyName = "privateKey";
-    private const string Algorithm = "algorithm";
+    private const string KeyUseName = "keyUse";
+    private const string PriorityName = "priority";
+    private const int DefaultKeySize = 2048;
+    private const int DefaultPriority = 100;
+
+    /// <summary>
+    /// The cached value of the public key retrieved from the configuration attributes.
+    /// </summary>
+    [NotMapped]
+    private string? _publicKey;
+    
+    /// <summary>
+    /// The cached value of the private key retrieved from the configuration attributes.
+    /// </summary>
+    [NotMapped]
+    private string? _privateKey;
+    
+    /// <summary>
+    /// The cached value of the key size retrieved from the configuration attributes.
+    /// </summary>
+    [NotMapped]
+    private int? _keySize;
+    
+    /// <summary>
+    /// The cached value of the priority retrieved from the configuration attributes.
+    /// </summary>
+    [NotMapped]
+    private int? _priortiy;
+    
     
     /// <summary>
     /// Initializes a new instance of the <see cref="RsaGenerationConfig"/> class with the specified identifier, realm identifier, and name.
@@ -24,37 +53,69 @@ public sealed class RsaGenerationConfig : Config
         : base(id, realmId, name, ProviderType.RsaKeyGen, new List<ConfigAttribute>())
     {
     }
-    
+
     /// <summary>
     /// Creates a new instance of <see cref="RsaGenerationConfig"/> with the specified configurations.
     /// </summary>
     /// <param name="realmId">The identifier of the realm to which the configuration belongs.</param>
     /// <param name="name">The name of the configuration.</param>
-    /// <param name="publicKey">The public key value.</param>
-    /// <param name="privateKey">The private key value.</param>
-    /// <param name="algorithm">The RSA algorithm.</param>
+    /// <param name="keySize">The key size of RSA</param>
+    /// <param name="priority">The priority of configuration</param>
     /// <returns>A new instance of <see cref="RsaGenerationConfig"/>.</returns>
-    public static RsaGenerationConfig CreateWithConfigurations(RealmId realmId, string name, string publicKey, string privateKey, string algorithm)
+    public static RsaGenerationConfig CreateWithConfigurations(RealmId realmId, string name, int keySize = DefaultKeySize, int priority = DefaultPriority)
     {
         var config = new RsaGenerationConfig(ConfigId.New(), realmId, name)
         {
             ConfigAttributes = new List<ConfigAttribute>()
         };
+
+        var privateKeyStr = CreateRsaPrivateKey(keySize);
         
-        config.AddConfigAttribute(PublicKeyName, publicKey);
-        config.AddConfigAttribute(PrivateKeyName, privateKey);
-        config.AddConfigAttribute(Algorithm, algorithm);
+        config.AddConfigAttribute(PrivateKeyName, privateKeyStr);
+        config.AddConfigAttribute(KeyUseName, KeyUse.Sig.Value);
+        config.AddConfigAttribute(PriorityName, priority.ToString());
 
         return config;
     }
 
+    private static string CreateRsaPrivateKey(int keySize)
+    {
+        using var rsa = RSA.Create(keySize);
+
+        var privateKey = rsa.ExportRSAPrivateKey();
+        var privateKeyStr = Convert.ToBase64String(privateKey);
+
+        return privateKeyStr;
+    }
+    
     /// <summary>
     /// Retrieves the public key from the configuration attributes.
     /// </summary>
     /// <returns>The public key.</returns>
     public string GetPublicKey()
     {
-        return ConfigAttributes.First(x => x.Name == PublicKeyName).Value;
+        if (_publicKey != null) return _publicKey;
+        
+        var privateKeyBytes = Convert.FromBase64String(GetPrivateKey());
+        
+        using var rsa = RSA.Create();
+        
+        rsa.ImportRSAPrivateKey(privateKeyBytes, out _);
+
+        var rsaParams = rsa.ExportParameters(true);
+
+        using var rsaPublic = RSA.Create(GetKeySize());
+        
+        rsaPublic.ImportParameters(rsaParams);
+
+        // Export the public key
+        var publicKeyBytes = rsaPublic.ExportRSAPublicKey();
+
+        // Convert the public key to base64 string
+        var publicKeyBase64 = Convert.ToBase64String(publicKeyBytes);
+
+        _publicKey = publicKeyBase64;
+        return _publicKey;
     }
 
     /// <summary>
@@ -63,17 +124,34 @@ public sealed class RsaGenerationConfig : Config
     /// <returns>The private key.</returns>
     public string GetPrivateKey()
     {
-        return ConfigAttributes.First(x => x.Name == PrivateKeyName).Value;
+        if (_privateKey != null) return _privateKey;
+        
+        _privateKey = ConfigAttributes.First(x => x.Name == PrivateKeyName).Value;
+        return _privateKey;
     }
-
+    
     /// <summary>
-    /// Retrieves the RSA algorithm from the configuration attributes.
+    /// Retrieves the priority from the configuration attributes.
     /// </summary>
-    /// <returns>The RSA algorithm.</returns>
-    public string GetAlgorithm()
+    /// <returns>The priority value.</returns>
+    public int GetPriority()
     {
-        return ConfigAttributes.First(x => x.Name == Algorithm).Value;
+        if (_priortiy != null) return _priortiy.Value;
+        _priortiy = Convert.ToInt32(ConfigAttributes.First(x => x.Name == PriorityName).Value);
+        return _priortiy.Value;
     }
+    
+    /// <summary>
+    /// Retrieves the key size from the configuration attributes.
+    /// </summary>
+    /// <returns>The key size value.</returns>
+    public int GetKeySize()
+    {
+        if (_keySize != null) return _keySize.Value;
+        _keySize = Convert.ToInt32(ConfigAttributes.First(x => x.Name == PriorityName).Value);
+        return _keySize.Value;
+    }
+    
 
     /// <summary>
     /// Retrieves the RSA parameters from the configuration attributes.
