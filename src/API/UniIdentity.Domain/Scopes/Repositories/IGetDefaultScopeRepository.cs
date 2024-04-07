ce9d04c5d2d6c0de6d93f2a6ab551921dea50e8f
emrecoskun705 <emrecoskun705@gmail.com>
2024-04-07 22:50:13 +0300

    add getter for default scope repository

---
+++ src/API/UniIdentity.Domain/Scopes/Repositories/IGetDefaultScopeRepository.cs
@@ -0,0 +1,17 @@
+﻿using UniIdentity.Domain.Realms;
+
+namespace UniIdentity.Domain.Scopes.Repositories;
+
+/// <summary>
+/// Represents a repository interface for retrieving default scopes associated with a realm.
+/// </summary>
+public interface IGetDefaultScopeRepository
+{
+    /// <summary>
+    /// Retrieves all default scopes associated with the specified realm ID.
+    /// </summary>
+    /// <param name="realmId">The ID of the realm to retrieve default scopes for.</param>
+    /// <param name="cancellationToken">The token to monitor for cancellation requests.</param>
+    /// <returns>A task that represents the asynchronous operation. The task result contains the collection of default scopes.</returns>
+    Task<IEnumerable<DefaultScope>> GetAllByRealmId(RealmId realmId, CancellationToken cancellationToken = default);
+}
--- src/API/UniIdentity.Infrastructure/Data/Repositories/DefaultScopeRepository.cs
+++ src/API/UniIdentity.Infrastructure/Data/Repositories/DefaultScopeRepository.cs
@@ -1,12 +1,22 @@
-﻿using UniIdentity.Domain.Scopes;
+﻿using Microsoft.EntityFrameworkCore;
+using UniIdentity.Domain.Realms;
+using UniIdentity.Domain.Scopes;
 using UniIdentity.Domain.Scopes.Repositories;
 
 namespace UniIdentity.Infrastructure.Data.Repositories;
 
-internal sealed class DefaultScopeRepository : Repository<DefaultScope>, IAddDefaultScopeRepository
+internal sealed class DefaultScopeRepository : Repository<DefaultScope>, IAddDefaultScopeRepository, IGetDefaultScopeRepository
 {
     public DefaultScopeRepository(ApplicationDbContext dbContext)
         : base(dbContext)
     {
+    }
+    
+    /// <inheritdoc />
+    public async Task<IEnumerable<DefaultScope>> GetAllByRealmId(RealmId realmId, CancellationToken cancellationToken = default)
+    {
+        return await _db.Set<DefaultScope>()
+            .Where(x => x.RealmId == realmId)
+            .ToListAsync(cancellationToken: cancellationToken);
     }
 }
