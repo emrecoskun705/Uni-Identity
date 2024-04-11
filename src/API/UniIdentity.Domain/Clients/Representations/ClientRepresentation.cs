4a33f03ba41efbc015d0256fe98cedefcb5241a0
emrecoskun705 <emrecoskun705@gmail.com>
2024-04-11 15:27:20 +0300

    add realm and client representations

---
+++ src/API/UniIdentity.Domain/Clients/Representations/ClientRepresentation.cs
@@ -0,0 +1,14 @@
+﻿using UniIdentity.Domain.ClientAttributes;
+using UniIdentity.Domain.Scopes;
+
+namespace UniIdentity.Domain.Clients.Representations;
+
+/// <summary>
+/// Represents a representation of a client including its all type of configurations.
+/// </summary>
+public class ClientRepresentation
+{
+    public required Client Client { get; init; }
+    public required List<DefaultScope> DefaultScopes { get; init; }
+    public required List<ClientAttribute> ClientAttributes { get; init; }
+}
---
+++ src/API/UniIdentity.Domain/Realms/Representations/RealmRepresentation.cs
@@ -0,0 +1,17 @@
+﻿using UniIdentity.Domain.Configs;
+using UniIdentity.Domain.RealmAttributes;
+using UniIdentity.Domain.Scopes;
+
+namespace UniIdentity.Domain.Realms.Representations;
+
+/// <summary>
+/// Represents a representation of a realm including its all type of configurations.
+/// </summary>
+public class RealmRepresentation
+{
+    public required Realm Realm { get; init; }
+    public required RealmAttribute AttributeSignatureAlgorithm { get; init; }
+    public required IReadOnlyList<Scope> Scopes { get; init; }
+    public required IReadOnlyList<DefaultScope> RealmDefaultScopes { get; init; }
+    public required IReadOnlyList<RsaGenerationConfig> RsaGenerationConfigs { get; init; }
+}
