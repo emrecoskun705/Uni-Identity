a149ddfa583e9a9ad4188138eafda567f712a2ab
emrecoskun705 <emrecoskun705@gmail.com>
2024-04-11 15:28:32 +0300

    update realm request command handler

--- src/API/UniIdentity.Application/Realms/Commands/AddRealmRequest/AddRealmRequestCommandHandler.cs
+++ src/API/UniIdentity.Application/Realms/Commands/AddRealmRequest/AddRealmRequestCommandHandler.cs
@@ -1,18 +1,16 @@
 ﻿using UniIdentity.Application.Contracts.Messaging;
 using UniIdentity.Domain;
-using UniIdentity.Domain.ClientAttributes.Repositories;
 using UniIdentity.Domain.Clients;
 using UniIdentity.Domain.Clients.Repositories;
+using UniIdentity.Domain.Clients.Services;
 using UniIdentity.Domain.Clients.ValueObjects;
 using UniIdentity.Domain.ClientScopes.Repositories;
 using UniIdentity.Domain.Common;
-using UniIdentity.Domain.Configs;
-using UniIdentity.Domain.Configs.Enums;
+using UniIdentity.Domain.Configs.Repositories;
 using UniIdentity.Domain.RealmAttributes.Repositories;
 using UniIdentity.Domain.Realms;
-using UniIdentity.Domain.Realms.Consts;
 using UniIdentity.Domain.Realms.Repositories;
-using UniIdentity.Domain.Scopes;
+using UniIdentity.Domain.Realms.Services;
 using UniIdentity.Domain.Scopes.Repositories;
 using UniIdentity.Domain.Shared;
 
@@ -23,32 +21,35 @@ internal sealed class AddRealmRequestCommandHandler : ICommandHandler<AddRealmRe
     private readonly IGetRealmRepository _getRealmRepository;
     private readonly IAddRealmRepository _addRealmRepository;
     private readonly IAddClientRepository _addClientRepository;
-    private readonly IAddClientAttributeRepository _addClientAttributeRepository;
     private readonly IAddClientScopeRepository _addClientScopeRepository;
+    private readonly IAddConfigRepository _addConfigRepository;
     private readonly IAddDefaultScopeRepository _addDefaultScopeRepository;
     private readonly IAddRealmAttributeRepository _addRealmAttributeRepository;
     private readonly IAddScopeRepository _addScopeRepository;
+    private readonly IGetDefaultScopeRepository _getDefaultScopeRepository;
     private readonly IUnitOfWork _unitOfWork;
 
     public AddRealmRequestCommandHandler(
         IGetRealmRepository getRealmRepository, 
         IAddRealmRepository addRealmRepository, 
         IAddClientRepository addClientRepository,
-        IAddClientAttributeRepository addClientAttributeRepository,
         IAddClientScopeRepository addClientScopeRepository,
+        IAddConfigRepository addConfigRepository,
         IAddDefaultScopeRepository addDefaultScopeRepository,
         IAddRealmAttributeRepository addRealmAttributeRepository, 
         IAddScopeRepository addScopeRepository,
+        IGetDefaultScopeRepository getDefaultScopeRepository,
         IUnitOfWork unitOfWork)
     {
         _getRealmRepository = getRealmRepository;
         _addRealmRepository = addRealmRepository;
         _addClientRepository = addClientRepository;
-        _addClientAttributeRepository = addClientAttributeRepository;
         _addClientScopeRepository = addClientScopeRepository;
+        _addConfigRepository = addConfigRepository;
         _addDefaultScopeRepository = addDefaultScopeRepository;
         _addRealmAttributeRepository = addRealmAttributeRepository;
         _addScopeRepository = addScopeRepository;
+        _getDefaultScopeRepository = getDefaultScopeRepository;
         _unitOfWork = unitOfWork;
     }
 
@@ -62,21 +63,30 @@ internal sealed class AddRealmRequestCommandHandler : ICommandHandler<AddRealmRe
         await using var transaction = await _unitOfWork.BeginTransactionAsync(cancellationToken);
         try
         {
-            var realm = Realm.Create(request.Name, request.Enabled);
+            var realmCreateService = new RealmCreateService(_addRealmRepository, _addRealmAttributeRepository,
+                _addScopeRepository, _addDefaultScopeRepository, _addConfigRepository);
+            
+            var clientCreateService = new ClientCreateService(_addClientRepository, _addClientScopeRepository,
+                _getDefaultScopeRepository);
+            
+            #region Realm Create
+            
+            var realmRepresentation = realmCreateService.AddRealmWithDefaults(request.Name, request.Enabled);
+            var realm = realmRepresentation.Realm;
 
-            _addRealmRepository.Add(realm);
+            #endregion
+            // save it because we would need data while creating client 
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
 
-            AddDefaultAttributes(realm);
+            #region Client Create
 
-            var scopes = AddRealmScopes(realm);
+            await clientCreateService.AddClientWithDefaults(realm.Id, ClientKey.FromValue("account"), Protocol.OpenIdConnect,
+                "/realms/" + realm.Id + "/account/", cancellationToken: cancellationToken);
 
-            var defaultScopes = AddRealmDefaultScopes(realm, scopes);
-
-            var clients = AddDefaultClients(realm);
-
-            AddDefaultClientScopes(clients, defaultScopes);
-
-            RsaGenerationConfig.CreateWithConfigurations(realm.Id, "default-rsa");
+            await clientCreateService.AddClientWithDefaults(realm.Id, ClientKey.FromValue("admin"), Protocol.OpenIdConnect,
+                "/admin/" + realm.Id + "/console/", cancellationToken);
+            
+            #endregion
 
             await _unitOfWork.SaveChangesAsync(cancellationToken);
 
@@ -90,52 +100,5 @@ internal sealed class AddRealmRequestCommandHandler : ICommandHandler<AddRealmRe
         
         return Result.Success();
     }
-
-    private void AddDefaultClientScopes(IEnumerable<Client> clients, IReadOnlyList<DefaultScope> defaultScopes)
-    {
-        foreach (var client in clients)
-        {
-            client.AddDefaultClientScopes(_addClientScopeRepository, defaultScopes);
-        }
-    }
-
-    private IReadOnlyList<Scope> AddRealmScopes(Realm realm)
-    {
-        return realm.AddScopes(_addScopeRepository);
-    }
-
-    private IReadOnlyList<DefaultScope> AddRealmDefaultScopes(Realm realm, IReadOnlyList<Scope> scopes)
-    {
-        return realm.AddDefaultScopes(_addDefaultScopeRepository, scopes);
-    }
-
-    private void AddDefaultAttributes(Realm realm)
-    {
-        var defaultSignatureAlgorithm =
-            realm.CreateAttribute(RealmAttributeName.SignatureAlgorithm, SignatureAlg.Default);
-        
-        _addRealmAttributeRepository.Add(defaultSignatureAlgorithm);
-    }
-
-    private List<Client> AddDefaultClients(Realm realm)
-    {
-        var clients = new List<Client>();
-        
-        var addAccountClient = Client.Create(realm.Id, ClientKey.FromValue("account"), Protocol.OpenIdConnect,
-            "/realms/" + realm.Id + "/account/");
-        
-        var addAdminClient = Client.Create(realm.Id, ClientKey.FromValue("admin"), Protocol.OpenIdConnect,
-            "/admin/" + realm.Id + "/console/");
-        
-        clients.Add(addAccountClient);
-        clients.Add(addAdminClient);
-        
-        _addClientRepository.Add(addAccountClient);
-        _addClientRepository.Add(addAdminClient);
-
-        addAccountClient.AddDefaultClientAttributes(_addClientAttributeRepository);
-        addAdminClient.AddDefaultClientAttributes(_addClientAttributeRepository);
-
-        return clients;
-    }
+    
 }
