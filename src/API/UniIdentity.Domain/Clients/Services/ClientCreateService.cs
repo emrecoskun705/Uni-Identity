5348ef8b8fdf4781d6c695c28394bc0df6d56a2a
emrecoskun705 <emrecoskun705@gmail.com>
2024-04-11 15:29:12 +0300

    add client create service

---
+++ src/API/UniIdentity.Domain/Clients/Services/ClientCreateService.cs
@@ -0,0 +1,35 @@
+﻿using UniIdentity.Domain.Clients.Repositories;
+using UniIdentity.Domain.Clients.ValueObjects;
+using UniIdentity.Domain.ClientScopes.Repositories;
+using UniIdentity.Domain.Realms;
+using UniIdentity.Domain.Scopes.Repositories;
+
+namespace UniIdentity.Domain.Clients.Services;
+
+public sealed class ClientCreateService
+{
+    private readonly IAddClientRepository _addClientRepository;
+    private readonly IAddClientScopeRepository _addClientScopeRepository;
+    private readonly IGetDefaultScopeRepository _getDefaultScopeRepository;
+
+    public ClientCreateService(
+        IAddClientRepository addClientRepository, 
+        IAddClientScopeRepository addClientScopeRepository, 
+        IGetDefaultScopeRepository getDefaultScopeRepository)
+    {
+        _addClientRepository = addClientRepository;
+        _addClientScopeRepository = addClientScopeRepository;
+        _getDefaultScopeRepository = getDefaultScopeRepository;
+    }
+
+    public async Task AddClientWithDefaults(RealmId realmId, ClientKey clientKey, Protocol protocol, string rootUrl, CancellationToken cancellationToken = default)
+    {
+        var client = Client.Create(realmId, clientKey, protocol, rootUrl);
+        _addClientRepository.Add(client);
+
+        var defaultScopes = (await _getDefaultScopeRepository.GetAllByRealmId(realmId, cancellationToken: cancellationToken)).ToList();
+        if (defaultScopes.Count == 0) throw new Exception("Default scope must contain some scope");
+
+        client.AddDefaultClientScopes(_addClientScopeRepository, defaultScopes);
+    }
+}
