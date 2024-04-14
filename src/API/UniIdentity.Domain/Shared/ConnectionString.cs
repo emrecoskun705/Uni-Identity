4afa71b0a0dd2941dad452477b3b73f0111e337c
emrecoskun705 <emrecoskun705@gmail.com>
2024-04-14 13:26:51 +0300

    update connection string summary

--- src/API/UniIdentity.Domain/Shared/ConnectionString.cs
+++ src/API/UniIdentity.Domain/Shared/ConnectionString.cs
@@ -1,14 +1,30 @@
 ﻿namespace UniIdentity.Domain.Shared;
 
+/// <summary>
+/// Represents a connection string for a database.
+/// </summary>
 public record ConnectionString
 {
+    /// <summary>
+    /// The default database connection string name.
+    /// </summary>
     public const string DefaultDb = "Default";
     
+    /// <summary>
+    /// Gets the value of the connection string.
+    /// </summary>
     public string Value { get; }
 
     private ConnectionString(string value) => Value = value;
 
+    /// <summary>
+    /// Gets the default database connection string.
+    /// </summary>
     public static ConnectionString DefaultDbConnectionString => new ConnectionString(DefaultDb);
 
+    /// <summary>
+    /// Implicitly converts a <see cref="ConnectionString"/> object to a <see cref="string"/>.
+    /// </summary>
+    /// <param name="connectionString">The connection string object to convert.</param>
     public static implicit operator string(ConnectionString connectionString) => connectionString.Value;
 }
