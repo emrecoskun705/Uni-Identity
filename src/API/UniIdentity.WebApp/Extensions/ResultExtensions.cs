7f765ae79a0757d5eb0dc44d8304a57d9a21460e
emrecoskun705 <emrecoskun705@gmail.com>
2024-04-21 14:30:55 +0300

    update result extensions

---
+++ src/API/UniIdentity.WebApp/Extensions/ResultExtensions.cs
@@ -0,0 +1,68 @@
+﻿using UniIdentity.Domain.Shared;
+
+namespace UniIdentity.WebApp.Extensions;
+
+public static class ResultExtensions
+{
+    public static IResult ToOk<TResult>(this Result<TResult> result)
+        => result.HandleFailure(() => Results.Ok(result.Value));
+    
+    public static IResult ToOk(this Result result)
+        => result.HandleFailure(() => Results.Ok());
+
+    public static IResult ToCreated(this Result result)
+        => result.HandleFailure(Results.Created);
+    
+
+    public static IResult ToProblemDetails(this Result result)
+    {
+        if (result.IsSuccess)
+            throw new InvalidOperationException();
+
+        var error = result.Error;
+        return Results.Problem(
+            statusCode: GetStatusCode(error.ErrorType),
+            title: GetTitle(error.ErrorType),
+            type: GetType(error.ErrorType),
+            extensions: new Dictionary<string, object?>()
+            {
+                { "errors", new[] { result.Error } }
+            }
+        );
+    }
+
+    static int GetStatusCode(ErrorType errorType) =>
+        errorType switch
+        {
+            ErrorType.Failure => StatusCodes.Status400BadRequest,
+            ErrorType.NotFound => StatusCodes.Status404NotFound,
+            ErrorType.Conflict => StatusCodes.Status409Conflict,
+            _ => StatusCodes.Status500InternalServerError
+        };
+
+    static string GetTitle(ErrorType errorType) =>
+        errorType switch
+        {
+            ErrorType.Failure => "Bad Request",
+            ErrorType.NotFound => "Not Found",
+            ErrorType.Validation => "Bad Request",
+            ErrorType.Conflict => "Conflict",
+            _ => "Server Failure"
+        };
+
+    static string GetType(ErrorType statusCode) =>
+        statusCode switch
+        {
+            ErrorType.Validation => "https://tools.ietf.org/html/rfc7231#section-6.5.1",
+            ErrorType.Failure => "https://tools.ietf.org/html/rfc7231#section-6.5.1",
+            ErrorType.NotFound => "https://tools.ietf.org/html/rfc7231#section-6.5.4",
+            ErrorType.Conflict => "https://tools.ietf.org/html/rfc7231#section-6.5.8",
+            _ => "https://tools.ietf.org/html/rfc7231#section-6.6.1"
+        };
+    
+
+    private static IResult HandleFailure(this Result result, Func<IResult> successAction)
+    {
+        return result.IsFailure ? result.ToProblemDetails() : successAction();
+    }
+}
